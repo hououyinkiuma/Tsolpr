b0bf584416ed49e2c48f143b3c3c2fd4d9014ac2
hououyinkiuma <31887370+hououyinkiuma@users.noreply.github.com>
2021-03-12 23:08:49 +0900

    Add files via upload

--- Tsolpr/main.cpp
+++ Tsolpr/main.cpp
@@ -1,4 +1,22 @@
 #include<iostream>
+#include"utils.h"
+#include<cstdlib>
+
+using namespace std;
+using namespace Tsolpr;
+
 int main(int argc, const char* argv[]) {
+	int t;
+
+}
+
+int TestFunction(const char* image_adress) {
+    auto files = Tsolpr::utils::get_files(image_adress);
+    if (files.size() == 0) {
+        cout << "files open error." << endl;
+        return 0;
+    }
+    cout << "test start.\n";
+
 
 }
